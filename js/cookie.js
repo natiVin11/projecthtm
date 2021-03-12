ddffe771b80f4d962edd4d08091f217aea03a60c
netiw <netiw29@gmail.com>
2021-03-12 13:46:50 +0200

    nati

--- js/cookie.js
+++ js/cookie.js
@@ -1,3 +1,15 @@
+const  DEFINTIONS={
+    USER_NAME:'username',
+    PASSWORD:'password'
+}
+changBackgroundColor=()=>{
+    setTimeout(()=>{
+
+    },3500)
+    setInterval(()=>{
+
+    },5000)
+}
 
 setCookie = (name, value, days) => {
     let d = new Date();
@@ -34,3 +46,23 @@ deleteCookie = (name, path) => {
     setCookie(name, '', -1, "=/")
 }
 
+getInputForUser=()=>{
+    let valueForUsername = document.getElementById("username");
+    let valueForFassworde = document.getElementById("pass");
+    if (valueForUsername.value.length>4){
+       setCookie(USER_NAME = valueForUsername.value,PASSWORD = valueForFassworde.value,45);
+    }
+    else {
+        alert("אנא בדוק את השם משתש והסיסמא והזן ערך תקין") ;
+    }
+}
+getChek=()=>{
+    let valueForUsername2 = document.getElementById("username2");
+    let valueForFassworde2 = document.getElementById("pass2");
+    if ( checkIfCookieExist(valueForUsername2 == useer)  && valueForFassworde2== valueForFassworde){
+        alert("המשתמש התחבר בהצחלה!");
+    }
+    else{
+        alert("נא לבדוק את הפרטים!");
+    }
+}
---
+++ js/js.js
@@ -0,0 +1,6 @@
+onclickorder=()=>{
+    alert("!המוצר נוסף לסל");
+}
+onclickconact=(onclick)=>{
+   alert("הפנייה הועברה לצוות פיצה טעמי, המשך יום טוב!");
+}
